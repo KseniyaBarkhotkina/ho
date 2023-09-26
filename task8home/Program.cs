8c55e3b055bb3ac0cd2e4fad06ed7822ecd72690
KseniyaBarkhotkina <barhotkinaks@mail.ru>
2023-09-26 22:10:26 +0300

    seminar1

---
+++ task8home/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = 2;
+if (number1 > 1)
+{
+    while (number2 <= number1)
+    {
+        Console.WriteLine(number2 + " ");
+        number2 = number2 + 2;
+    }
+}
